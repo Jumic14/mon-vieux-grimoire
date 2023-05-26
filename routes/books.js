9eec2ec8894ecd2f748e4ced3038fb424c0c7995
Julien <michel-julien1@hotmail.fr>
2023-05-26 17:43:50 +0200

    Optimize controllers structure

--- routes/books.js
+++ routes/books.js
@@ -1,27 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const Book = require('../models/Book');
+const booksCtrl = require('../controllers/books')
 
-router.post('/', (req, res, next) => {
-    const book = new Book({
-      ...req.body
-    });
-    book.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-router.get('/:id', (req, res, next) => {
-Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
-});
-
-router.get('/', (req, res, next) => {
-Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({ error }));
-});
+router.get('/', booksCtrl.getAllBooks);
+router.post('/', booksCtrl.createBook);
+router.get('/:id', booksCtrl.getOneBook);
+router.put('/:id', booksCtrl.modifyBook);
+router.delete('/:id', booksCtrl.deleteBook);
 
 module.exports = router; 
