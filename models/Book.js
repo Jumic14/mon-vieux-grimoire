957d61661ff7e761f3aa4aa0236c17ab4bed8d8c
Julien <michel-julien1@hotmail.fr>
2023-06-04 15:38:40 +0200

    Update models

--- models/Book.js
+++ models/Book.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
+  userId : { type: String, required: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
-  year: { type: String, required: true },
   imageUrl: { type: String, required: true },
-  price: { type: Number, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  ratings: [{
+    userId : {type: String},
+    grade : {type: Number}, 
+  }],
+  averageRating: {type: Number},
+  id: {type: String}
 });
 
 module.exports = mongoose.model('Book', bookSchema);
