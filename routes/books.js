9a3fda2f3b72e277fbfba77649ea8af59e59fe9a
Julien <michel-julien1@hotmail.fr>
2023-06-04 15:39:08 +0200

    Update routes

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const booksCtrl = require('../controllers/books')
+const auth = require('../middleware/auth');
+const multer = require ('../middleware/multer-config');
+
+const booksCtrl = require('../controllers/books');
 
 router.get('/', booksCtrl.getAllBooks);
-router.post('/', booksCtrl.createBook);
+router.post('/', auth, multer, booksCtrl.createBook);
 router.get('/:id', booksCtrl.getOneBook);
-router.put('/:id', booksCtrl.modifyBook);
-router.delete('/:id', booksCtrl.deleteBook);
+router.put('/:id', auth, multer, booksCtrl.modifyBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+router.post('/:id/rating', auth, booksCtrl.rateBook);
 
 module.exports = router; 
