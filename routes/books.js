a830f2c30440313b8edb22a86db0351a812e3dbb
Julien <michel-julien1@hotmail.fr>
2023-06-06 17:37:29 +0200

    Green code optimizations

--- routes/books.js
+++ routes/books.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-const multer = require ('../middleware/multer-config');
+const SharpMulter = require ('../middleware/multer-config');
 
 const booksCtrl = require('../controllers/books');
 
 router.get('/', booksCtrl.getAllBooks);
-router.post('/', auth, multer, booksCtrl.createBook);
+router.post('/', auth, SharpMulter, booksCtrl.createBook);
 router.get('/bestrating', booksCtrl.getBestRatingBooks);
 router.get('/:id', booksCtrl.getOneBook);
-router.put('/:id', auth, multer, booksCtrl.modifyBook);
+router.put('/:id', auth, SharpMulter, booksCtrl.modifyBook);
 router.delete('/:id', auth, booksCtrl.deleteBook);
 router.post('/:id/rating', auth, booksCtrl.rateBook);
 
